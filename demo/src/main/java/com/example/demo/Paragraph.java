6626a0b22f5a5837ba91b4947795d5a1afa6a072
PetcuBogdan <bogdan.petcu02@e-uvt.ro>
2023-10-23 10:41:47 +0300

    lab3

--- demo/src/main/java/com/example/demo/Paragraph.java
+++ demo/src/main/java/com/example/demo/Paragraph.java
@@ -3,15 +3,29 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Paragraph {
+public class Paragraph implements Element{
     private String text;
-
 
     public Paragraph(String text) {
         this.text = text;
     }
 
-    void print() {
+    public void print() {
         System.out.println("Paragraph: " + text);
+    }
+
+    @Override
+    public void add(Element el) {
+
+    }
+
+    @Override
+    public Element get(int nr) {
+        return null;
+    }
+
+    @Override
+    public void remove(Element el){
+
     }
 }
